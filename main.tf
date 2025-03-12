6e5d9ceb35f062961aee858a76f5f66b8e506f64
mubin-ifi <91716677+mubin-puc@users.noreply.github.com>
2025-03-12 20:13:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,69 +1,31 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.22.0"
+resource "azurerm_container_app" "containerapp" {
+  name                         = var.container_apps_sample_app_name
+  container_app_environment_id = azurerm_container_app_environment.environment.id
+  resource_group_name          = azurerm_resource_group.example.name
+  revision_mode                = "Multiple"
+
+  template {
+    container {
+      name   = "adoagent"
+      image  = "locals things"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      volume_mounts {
+        name = "azure-files-volume"
+        path = "/app/images"
+      }
     }
 
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~>0.5.0"
+    volume {
+      name         = "azure-files-volume"
+      storage_type = "AzureFile"
+      storage_name = azurerm_container_app_environment_storage.env_storage.name
     }
+
+    min_replicas = 0
+    max_replicas = 5
   }
 
 
-# creating aca environment
-resource "azapi_resource" "aca_env" {
-  type      = "Microsoft.App/managedEnvironments@2022-03-01"
-  parent_id = azurerm_resource_group.rg.id
-  location  = azurerm_resource_group.rg.location
-  name      = "aca-env-terraform"
-
-  body = jsonencode({
-    properties = {
-      appLogsConfiguration = {
-        destination = "log-analytics"
-        logAnalyticsConfiguration = {
-          customerId = azurerm_log_analytics_workspace.law.workspace_id
-          sharedKey  = azurerm_log_analytics_workspace.law.primary_shared_key
-        }
-      }
-    }
-  })
-}
-
-# creating the aca
-resource "azapi_resource" "aca" {
-  type      = "Microsoft.App/containerApps@2022-03-01"
-  parent_id = azurerm_resource_group.rg.id
-  location  = azurerm_resource_group.rg.location
-  name      = "terraform-app"
-
-  body = jsonencode({
-    properties : {
-      managedEnvironmentId = azapi_resource.aca_env.id
-      configuration = {
-        ingress = {
-          external   = true
-          targetPort = 80
-        }
-      }
-      template = {
-        containers = [
-          {
-            name  = "web"
-            image = "nginx"
-            resources = {
-              cpu    = 0.5
-              memory = "1.0Gi"
-            }
-          }
-        ]
-        scale = {
-          minReplicas = 2
-          maxReplicas = 20
-        }
-      }
-    }
-  })
 }
